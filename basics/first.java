0c9b22eb9a7cf1e2152a67ca1d5c369d61e483cd
Astha <rathoreastha12@gmail.com>
2026-02-12 09:59:09 +0530

    initial commit

---
+++ basics/first.java
@@ -0,0 +1,66 @@
+package basics;
+
+public class first {
+    public static void main(String[] args) {
+
+// ------basic print statement----------------
+        // System.out.println("Hello");
+        // System.out.println("astha");
+        // System.out.println("ayush");
+        // System.out.println("PRACHI\n and  kajal");
+
+// ---------variables------------
+        // int a =10;
+        // System.out.println(a);
+        // int b;
+        // b=20;
+        // System.out.println(b);
+        // Scanner sc= new Scanner(System.in);
+        // System.out.println("enter a number");
+        // int a = sc.nextInt();
+        // System.out.println("the square of the number is");
+
+ // ---------sum of three numbers----------------
+         // System.out.println(a*a);
+        // System.out.println("enter first number");
+        // int a = sc.nextInt();
+        // System.out.println("enter second number");
+        // int b = sc.nextInt();
+        // System.out.println("enter third number");
+        // int c = sc.nextInt();
+        // System.out.println("the sum of three number is " + (a+b+c));
+
+// ---------- taking string as input-----------------
+        // System.out.println("enter your name");
+        // String  name = sc.nextLine();
+        // System.out.println("enter you school name");
+        // String school = sc.nextLine();
+
+// ----------printing ASCII value of a character-----------------
+        // char ch = 'A';
+        // int x = ch;
+        // System.out.println(x);
+
+// ----------conditional statements-------
+//  Scanner sc = new Scanner(System.in);
+       
+//   System.out.println("enter first number");
+//         int num = sc.nextInt();
+//         if(num%2==0){
+//                 System.out.println("this number is even number");
+//         }else{
+//                 System.out.println("this number is odd number");
+
+//         }
+// }
+  
+//   System.out.println("enter first number");
+//         int num = sc.nextInt();
+//         if(num%5==0){
+//                 System.out.println("this number is divisible by 5");
+//         }else{
+//                 System.out.println("this number is not divisible by 5");
+
+       }
+
+}
