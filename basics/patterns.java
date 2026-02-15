e477a95c66f429abf202f5ce85e1bb5bfcf8411c
Astha <rathoreastha12@gmail.com>
2026-02-16 01:16:10 +0530

    patterns file

---
+++ basics/patterns.java
@@ -0,0 +1,47 @@
+package basics;
+
+import java.util.Scanner;
+
+public class patterns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        // int b = sc.nextInt();
+
+// ---------right angle triangle star pattern----------
+        // for(int i =1; i<=a; i++){
+        //     for(int j =1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();   
+        // }
+
+// ------------ single line of stars--------
+        // for(int i =1; i<=a; i++){
+        //     for(int j =1; j<=i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();   
+        // }
+   
+// ---------when no of rows and columns are different----------
+    // for(int i =1; i<=a; i++){
+    //         for(int j =1; j<=b; j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();   
+    //     }   
+   
+// ---------square star---------
+    //   for(int i =1; i<=a; i++){
+    //         for(int j =1; j<=a; j++){
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();   
+    //     } 
+
+    
+        }
+    }
+
+
