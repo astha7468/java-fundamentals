4564cb2d420cf224667b7b10f84034191161f640
Astha <rathoreastha12@gmail.com>
2026-03-28 01:01:14 +0530

    update logic

--- basics/methods.java
+++ basics/methods.java
@@ -1,17 +1,62 @@
 package basics;
+
 public class methods{
-    public static void Astha(){
-        prachi();                  //LINE 5
-        System.out.println("hello"); //LINE 7
-    }
-    public static void main(String[]args){  //LINE 1
-        Ayush();                           //LINE 2
-    }
-    public static void prachi(){
-        System.out.println("how are you"); //LINE 6
-    }
-    public static void Ayush(){
-        System.out.println("I am fine");// LINE 3
-        Astha();                        // LINE 4
-    }
+//! basics understanding of methods/functions
+    // public static void Astha(){
+    //     prachi();                  //LINE 5
+    //     System.out.println("hello"); //LINE 7
+    // }
+    // public static void main(String[]args){  //LINE 1
+    //     Ayush();                           //LINE 2
+    // }
+    // public static void prachi(){
+    //     System.out.println("how are you"); //LINE 6
+    // }
+    // public static void Ayush(){
+    //     System.out.println("I am fine");// LINE 3
+    //     Astha();                        // LINE 4
+    // }
+
+//! sum function
+// public static void sum(int a, int b){
+//     System.out.println(a + b);
+// }
+// public static void main(String[] args) {
+//     sum(10, 20);
+// }
+
+//! function to find maximum of three numbers
+// public static void max(int a,int b, int c){
+//   if(a>b && a>c){
+//     System.out.println(a);
+//   }
+//   else if(b>c && b>a){
+//     System.out.println(b);
+//   }
+//   else{
+//     System.out.println(c);
+//   }
+// }
+// public static void main(String[]args){
+//     max(9, 5, 10);
+// }
+
+//! function to find minimum of three numbers
+// public static void min(int a,int b, int c){
+//   if(a<b && a<c){
+//     System.out.println(a);
+//   }
+//   else if(b<c && b<a){
+//     System.out.println(b);
+//   }
+//   else{
+//     System.out.println(c);
+//   }
+// }
+// public static void main(String[]args){
+//     min(9, 5, 10);
+// }
+
 }
+
+
