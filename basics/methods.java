25596898d4c4b600240eb718126749762ae04a05
Astha <rathoreastha12@gmail.com>
2026-03-28 23:54:14 +0530

    update changes

--- basics/methods.java
+++ basics/methods.java
@@ -2,7 +2,7 @@ package basics;
 
 public class methods{
 
-//---------- basics understanding of methods/functions--------------
+//!---------- basics understanding of methods/functions--------------
     // public static void Astha(){
     //     prachi();                  //LINE 5
     //     System.out.println("hello"); //LINE 7
@@ -18,7 +18,7 @@ public class methods{
     //     Astha();                        // LINE 4
     // }
 
-//---------- sum function---------------
+//!---------- sum function---------------
 // public static void sum(int a, int b){
 //     System.out.println(a + b);
 // }
@@ -26,7 +26,7 @@ public class methods{
 //     sum(10, 20);
 // }
 
-//-------------function to find maximum of three numbers-------------
+//!-------------function to find maximum of three numbers-------------
 // public static void max(int a,int b, int c){
 //   if(a>b && a>c){
 //     System.out.println(a);
@@ -42,7 +42,7 @@ public class methods{
 //     max(9, 5, 10);
 // }
 
-//-------------function to find minimum of three numbers-------------
+//!-------------function to find minimum of three numbers-------------
 // public static void min(int a,int b, int c){
 //   if(a<b && a<c){
 //     System.out.println(a);
@@ -58,7 +58,7 @@ public class methods{
 //     min(9, 5, 10);
 // }
 
- //---------------return type-----------------
+ //!---------------return type-----------------
 // public static int Astha(){     
 //     System.out.println("hello");
 //     return 8;
@@ -68,8 +68,62 @@ public class methods{
 // }
 // here ASTHA  is using as a function which is returning an integer value 8 and also printing "hello" when called in the main method.
 
+//!----------------max of three numbers using return type----------------
+// public static int max(int a,int b,int c){
+//     if(a>b && a>c){
+//         return a;
+//     }
+//     else if(b>c && b>a){
+//         return b;
+//     }
+//     else{
+//         return c;
+//     }
+// }
+// public static void main(String[]args){
+//     System.out.println(max(2,3,4));
+// }
+
+
+//!------------random question-----------
+// public static void printingnumbers(int n){
+//     for (int i =0; i<=n; i++){
+//         System.out.println(i);
+//     }
+// }
+// public static void main(String[]args){
+//     printingnumbers(8);
+// }
+
+//!---------sum of three numbers-------
+// public static int sum(int a, int b, int c){
+//     return a+b+c;
+// }
+// public static void main(String[]args){
+//     System.out.println(sum(10, 20, 30));
+// }
+
+// !---------factorial of a number-------
+// public static int fact(int n){
+//     if(n == 0){
+//         return 1;
+//     }
+//     return n * fact(n-1);
+// }
+// public static void main(String[]args){
+//     System.out.println(fact(5));
+// }
+
+//!---------two methods-----------
+// public static void sum(int a, int b){
+//     System.out.println(a+b);
+// }
+// public static void product(int a, int b){
+//     System.out.println(a*b);
+// }
+// public static void main(String[]args){
+//     sum(10, 20);
+//     product(10, 20);
+// }
 
 }
-
-
-
