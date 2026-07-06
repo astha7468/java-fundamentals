23f906b460d05d72b5b26adef43862a0bad4dc95
Astha <rathoreastha12@gmail.com>
2026-07-07 00:12:49 +0530

    inital commit

--- basics/Array.java
+++ basics/Array.java
@@ -226,23 +226,32 @@ public class Array {
 
 
 //!-----------REVERSE  ARRAY-----------
-        int [] arr = {23,3,45,7,89};
-        int n = arr.length;
-        int i =0;
-        int j=n-1;
-        while (i<j){
-                int temp =arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
-                i++;
-                j--;
-        }
-        for(int ele:arr){
-                System.out.println(ele);
+        // int [] arr = {23,3,45,7,89};
+        // int n = arr.length;
+        // int i =0;
+        // int j=n-1;
+        // while (i<j){
+        //         int temp =arr[i];
+        //         arr[i]=arr[j];
+        //         arr[j]=temp;
+        //         i++;
+        //         j--;
+        // }
+        // for(int ele:arr){
+        // System.out.println(ele);
 
-        }
-    }
-}
+        // }
+
+
+
+
+      
+   
+
+
+
+       
+
 
 
        
--- basics/ArrayList.java
+++ basics/ArrayList.java
@@ -2,10 +2,46 @@ package basics;
 
 public class ArrayList {
     public static void main(String[] args) {
-        java.util.ArrayList<Integer> list = new java.util.ArrayList<Integer>();
-        list.add(23);
-        System.out.println(list.get(0));
-        
+    //     java.util.ArrayList<Integer> list = new java.util.ArrayList<Integer>();
+    //     list.add(23);
+    //     list.add(45);
+    //     list.add(0,399);
+    //     System.out.println(list.get(0));
+    //     System.out.println(list.get(1));
+    //     System.out.println(list.get(2));
+    // }
+
+//!---------MERGE TWO SORTED ARRAYS------------
+      int[] a = {1,2,3,4};
+      int[] b = {6,7,8,9,10};
+      int[] c = new int[a.length+b.length];
+      int i = 0;
+      int j = 0;
+      int k = 0;
+      while(i < a.length && j < b.length){
+         if(a[i] < b[j]){
+                c[k] = a[i];
+                i++;
+         } else {
+                c[k] = b[j];
+                j++;
+         }
+         k++;
+      }
+      while(i < a.length){
+         c[k] = a[i];
+         i++;
+         k++;
+      }
+      while(j < b.length){
+         c[k] = b[j];
+         j++;
+         k++;
+      }
+      System.out.println("Merged array: ");
+      for(int ele : c){
+         System.out.println(ele);
+      }
     }
 }
 
