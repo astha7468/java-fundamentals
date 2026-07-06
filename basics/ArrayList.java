2a51c0b6d6e0e0bd11797cbbab0cf0823d8948ec
Astha <rathoreastha12@gmail.com>
2026-07-07 00:25:23 +0530

    inital changes

--- basics/ArrayList.java
+++ basics/ArrayList.java
@@ -11,37 +11,71 @@ public class ArrayList {
     //     System.out.println(list.get(2));
     // }
 
-//!---------MERGE TWO SORTED ARRAYS------------
-      int[] a = {1,2,3,4};
-      int[] b = {6,7,8,9,10};
-      int[] c = new int[a.length+b.length];
-      int i = 0;
-      int j = 0;
-      int k = 0;
-      while(i < a.length && j < b.length){
-         if(a[i] < b[j]){
-                c[k] = a[i];
-                i++;
-         } else {
-                c[k] = b[j];
-                j++;
-         }
-         k++;
-      }
-      while(i < a.length){
-         c[k] = a[i];
-         i++;
-         k++;
-      }
-      while(j < b.length){
-         c[k] = b[j];
-         j++;
-         k++;
-      }
-      System.out.println("Merged array: ");
-      for(int ele : c){
-         System.out.println(ele);
-      }
+//!---------MERGE TWO SORTED ARRAYS  ------------
+    //   int[] a = {1,2,3,4};
+    //   int[] b = {6,7,8,9,10};
+    //   int[] c = new int[a.length+b.length];
+    //   int i = 0;
+    //   int j = 0;
+    //   int k = 0;
+    //   while(i < a.length && j < b.length){
+    //      if(a[i] < b[j]){
+    //             c[k] = a[i];
+    //             i++;
+    //      } else {
+    //             c[k] = b[j];
+    //             j++;
+    //      }
+    //      k++;
+    //   }
+    //   while(i < a.length){
+    //      c[k] = a[i];
+    //      i++;
+    //      k++;
+    //   }
+    //   while(j < b.length){
+    //      c[k] = b[j];
+    //      j++;
+    //      k++;
+    //   }
+    //   System.out.println("Merged array: ");
+    //   for(int ele : c){
+    //      System.out.println(ele);
+    //   }
+
+
+//!---------MERGE TWO SORTED ARRAYS IN DESCENDING ORDER  ------------
+    // int[] a = {1,2,3,4};
+    //   int[] b = {6,7,8,9,10};
+    //   int[] c = new int[a.length+b.length];
+    //   int i = a.length-1;
+    //   int j = b.length-1;
+    //   int k = a.length+b.length-1;
+    //   while(i >= 0 && j >= 0){
+    //      if(a[i] >b[j]){
+    //             c[k] = a[i];
+    //             i--;
+    //      } else {
+    //             c[k] = b[j];
+    //             j--;
+    //      }
+    //      k--;
+    //   }
+    //   while(i >= 0){
+    //      c[k] = a[i];
+    //      i--;
+    //      k--;
+    //   }
+    //   while(j >= 0){
+    //      c[k] = b[j];
+    //      j--;
+    //      k--;
+    //   }
+    //   System.out.println("Merged array: ");
+    //   for(int ele : c){
+    //      System.out.println(ele);
+    //   }
+
     }
 }
 
